0a8644d87e3f079feb789e809cdf7e32aa90fb11
Edwin <edwingenova@gmail.com>
2020-03-15 18:29:33 -0400

    remove gitignore files

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO burgers (name) VALUES ('Meeses');
-INSERT INTO burgers (name) VALUES ('Bobbi');
-INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO burgers (name) VALUES ('Owens');
+INSERT INTO burgers(burger_name) VALUES(Cheesburger);
+INSERT INTO burgers(burger_name) VALUES(Spicy Chicken); 
+INSERT INTO burgers(burger_name) VALUES(Bacon Burger); 
